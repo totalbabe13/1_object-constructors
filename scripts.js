a669fe9f7dee41dc7b37084f600589c0f989d1b4
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-10-03 21:25:48 -0500

    added event listener to form for user input

--- scripts.js
+++ scripts.js
@@ -1,18 +1,40 @@
 
 
 let myLibrary = [];
+let submitBookForm = document.getElementById("book-form");
+submitBookForm.addEventListener("submit", getFormInput);
+
+function getFormInput(event) {
+  console.log(this);
+  event.preventDefault();
+  let currentTitle  = this.querySelector('[name=title-x]').value;
+  let currentAuthor = this.querySelector('[name=author-x]').value;
+  let currentPages  = this.querySelector('[name=pages-x]').value;
+  let currentRead   = this.querySelector('[name=read-x]').value
+  //console.log(` title is: ${currentTitle}, author is: ${currentAuthor}, pages is: ${currentPages}, have read? ${currentRead} `);
+
+  const userBook = new Book(currentTitle,currentAuthor,currentPages,currentRead);
+  console.log(userBook);
+
+};
 
 function Book(title, author, num_page, have_read) {
   this.title     = title
   this.author    = author
   this.num_page  = num_page
   this.have_read = have_read
-
-  this.info = function() {
-    return `${this.title}, ${this.author}, ${this.num_page}, ${this.have_read}.`
-    // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-  }
+  // this.info = function() {
+  //   return `${this.title}, ${this.author}, ${this.num_page}, ${this.have_read}.`
+  //   // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+  // }
 }
 
-const bookTest = new Book("The Hobbit","J.R.R.","295 pages","not read yet")
-bookTest.info();
+Book.prototype.info = function () {
+    return `${this.title}, ${this.author}, ${this.num_page}, ${this.have_read}.`
+};
+
+function addBookToLibrary(userBook) {
+  myLibrary.push(userBook)
+};
+
+//const bookTest = new Book("The Hobbit","J.R.R.","295 pages","not read yet")
