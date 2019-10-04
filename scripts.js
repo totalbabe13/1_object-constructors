cafb5dbe2281cade9ca35df5da10b17e8f28baff
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-10-04 16:35:19 -0500

    created html template for book cards

--- scripts.js
+++ scripts.js
@@ -5,17 +5,17 @@ let submitBookForm = document.getElementById("book-form");
 submitBookForm.addEventListener("submit", getFormInput);
 
 function getFormInput(event) {
-  console.log(this);
+  //console.log(this); !!LOOK:
+  //this inside this function points at the HTML element that has event listener(FORM)
   event.preventDefault();
   let currentTitle  = this.querySelector('[name=title-x]').value;
   let currentAuthor = this.querySelector('[name=author-x]').value;
   let currentPages  = this.querySelector('[name=pages-x]').value;
   let currentRead   = this.querySelector('[name=read-x]').value
   //console.log(` title is: ${currentTitle}, author is: ${currentAuthor}, pages is: ${currentPages}, have read? ${currentRead} `);
-
   const userBook = new Book(currentTitle,currentAuthor,currentPages,currentRead);
   console.log(userBook);
-
+  addBookToLibrary(userBook);
 };
 
 function Book(title, author, num_page, have_read) {
@@ -23,11 +23,7 @@ function Book(title, author, num_page, have_read) {
   this.author    = author
   this.num_page  = num_page
   this.have_read = have_read
-  // this.info = function() {
-  //   return `${this.title}, ${this.author}, ${this.num_page}, ${this.have_read}.`
-  //   // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-  // }
-}
+};
 
 Book.prototype.info = function () {
     return `${this.title}, ${this.author}, ${this.num_page}, ${this.have_read}.`
