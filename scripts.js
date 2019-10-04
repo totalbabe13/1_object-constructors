499807631b8ef4c2e500595e7803d8207bbadd70
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-10-04 17:01:23 -0500

    creating a rendering html function in JS

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 
 
-let myLibrary = [];
-let submitBookForm = document.getElementById("book-form");
-submitBookForm.addEventListener("submit", getFormInput);
+let   myLibrary = [];
+const cardsContainer = document.querySelector(".cardsContainer");
+let   submitBookForm = document.getElementById("bookForm");
+      submitBookForm.addEventListener("submit", getFormInput);
 
 function getFormInput(event) {
   //console.log(this); !!LOOK:
@@ -11,8 +12,8 @@ function getFormInput(event) {
   let currentTitle  = this.querySelector('[name=title-x]').value;
   let currentAuthor = this.querySelector('[name=author-x]').value;
   let currentPages  = this.querySelector('[name=pages-x]').value;
-  let currentRead   = this.querySelector('[name=read-x]').value
-  //console.log(` title is: ${currentTitle}, author is: ${currentAuthor}, pages is: ${currentPages}, have read? ${currentRead} `);
+  let currentRead   = this.querySelector('[name=read-x]').value;
+
   const userBook = new Book(currentTitle,currentAuthor,currentPages,currentRead);
   console.log(userBook);
   addBookToLibrary(userBook);
@@ -33,4 +34,10 @@ function addBookToLibrary(userBook) {
   myLibrary.push(userBook)
 };
 
+
+
+
+let   bookCard = document.createElement("div");
+      bookCard.className = "bookCard";
+      cardsContainer.appendChild(bookCard);
 //const bookTest = new Book("The Hobbit","J.R.R.","295 pages","not read yet")
