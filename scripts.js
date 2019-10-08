14cfba164550a3b790356302172c8bac87b4c677
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-10-07 19:55:07 -0500

    updated JS to format books

--- scripts.js
+++ scripts.js
@@ -10,6 +10,10 @@ document.getElementById("plus").addEventListener("click",function(){
 document.querySelector(".modal").style.display = 'flex';
 });
 
+document.getElementById("cancel").addEventListener("submit",function(){
+  document.querySelector(".modal").style.display = 'none';
+})
+
 function getFormInput(event) {
   //console.log(this); !!LOOK:
   //this inside this function points at the HTML element that has event listener(FORM)
@@ -20,6 +24,7 @@ function getFormInput(event) {
   const userBook = new Book(currentTitle,currentAuthor,currentPages);
   addBookToLibrary(userBook);
   document.querySelector(".modal").style.display = 'none';
+  formatBookObject();
 };
 
 function Book(title, author, num_page) {
@@ -44,38 +49,44 @@ myLibrary.forEach(function(book) {
   let bookCard = document.createElement("div");
     bookCard.className = "bookCard";
     cardsContainer.appendChild(bookCard);
+// < - - Title ELEMENT - - >
+  let actualTitle = document.createElement("h2");
+  let divider     = document.createElement("p");
 
-  let bookTitleContainer = document.createElement("div");
-  let actualTitle = document.createElement("span");
-    bookTitleContainer.className = "titleContainer";
-    actualTitle.className = "titleCard";
-    bookCard.appendChild(bookTitleContainer);
-    bookTitleContainer.appendChild(actualTitle);
-    actualTitle.textContent = `Title: ${book.title}`;
+    actualTitle.className = "title-tag";
+    divider.className     = "divider"
 
+    bookCard.appendChild(actualTitle);
+    bookCard.appendChild(divider);
+    actualTitle.textContent = `${book.title}`;
+    divider.textContent = "___________"
+
+// < - - Author ELEMENT - - >
   let bookAuthorContainer = document.createElement("div");
-  let actualAuthor = document.createElement("span");
+  let actualAuthor        = document.createElement("span");
+
     bookAuthorContainer.className = "authorContainer";
-    actualAuthor.className = "authorCard";
+    actualAuthor.className        = "author-tag";
+
     bookCard.appendChild(bookAuthorContainer);
     bookAuthorContainer.appendChild(actualAuthor);
-    actualAuthor.textContent = `Author: ${book.author}`;
-
+    actualAuthor.textContent = `${book.author}`;
+// < - - Pages ELEMENT - - >
   let bookPagesContainer = document.createElement("div");
   let actualPages = document.createElement("span");
     bookPagesContainer.className = "pagesContainer";
-    actualPages.className = "pagesCard";
+    actualPages.className = "pages-tag";
     bookCard.appendChild(bookPagesContainer);
     bookPagesContainer.appendChild(actualPages);
-    actualPages.textContent = `Num. of Pages: ${book.num_page}`;
+    actualPages.textContent = `${book.num_page} pages`;
 
-  let bookReadContainer = document.createElement("div");
-  let actualRead = document.createElement("span");
-    bookReadContainer.className = "readContainer";
-    actualRead.className = "readCard";
-    bookCard.appendChild(bookReadContainer);
-    bookReadContainer.appendChild(actualRead);
-    actualRead.textContent = `Have read: ${book.have_read}`;
+  // let bookReadContainer = document.createElement("div");
+  // let actualRead = document.createElement("span");
+  //   bookReadContainer.className = "readContainer";
+  //   actualRead.className = "readCard";
+  //   bookCard.appendChild(bookReadContainer);
+  //   bookReadContainer.appendChild(actualRead);
+  //   actualRead.textContent = `Have read: ${book.have_read}`;
 
   });
 };
