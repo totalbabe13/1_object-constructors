e6026b0c7bf466476ad7a9d4d2ced3025436d004
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-10-08 13:47:53 -0500

    cleaned up JS code functionality

--- scripts.js
+++ scripts.js
@@ -1,31 +1,14 @@
 
-
-let   myLibrary = [];
-const cardsContainer = document.querySelector(".cardsContainer");
-let   submitBookForm = document.getElementById("book-form");
-      submitBookForm.addEventListener("submit", getFormInput);
-
-
-document.getElementById("plus").addEventListener("click",function(){
-document.querySelector(".modal").style.display = 'flex';
-});
-
-document.getElementById("cancel").addEventListener("submit",function(){
-  document.querySelector(".modal").style.display = 'none';
-})
-
-function getFormInput(event) {
-  //console.log(this); !!LOOK:
-  //this inside this function points at the HTML element that has event listener(FORM)
-  event.preventDefault();
-  let currentTitle  = this.querySelector('[name=title-x]').value;
-  let currentAuthor = this.querySelector('[name=author-x]').value;
-  let currentPages  = this.querySelector('[name=pages-x]').value;
-  const userBook = new Book(currentTitle,currentAuthor,currentPages);
-  addBookToLibrary(userBook);
-  document.querySelector(".modal").style.display = 'none';
-  formatBookObject();
-};
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//--> OBJECTS and Prototypes
+let   myLibrary      = [];
+let   body           = document.getElementById("body");
+let   addBookButton  = document.getElementById("plus");
+let   modal          = document.getElementById("modal");
+let   bookForm       = document.getElementById('book-form');
+let   cardsContainer = document.getElementById("cardsContainer");
+let   saveButton     = document.getElementById("save");
+let   cancelButton   = document.getElementById("cancel");
 
 function Book(title, author, num_page) {
   this.title     = title
@@ -36,19 +19,51 @@ function Book(title, author, num_page) {
 Book.prototype.info = function () {
   return `${this.title}, ${this.author}, ${this.num_page}, ${this.have_read}.`
 };
-
 function addBookToLibrary(userBook) {
   myLibrary.push(userBook);
 };
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//EVENT Listenners -->
+addBookButton.addEventListener("click",showModal);
+bookForm.addEventListener("submit",saveBook);
+bookForm.addEventListener("reset",closeModal);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// MODAL open/closeModal
+function showModal(event){
+  document.querySelector(".modal").style.display = 'flex';
+};
+
+function closeModal(event){
+  document.querySelector(".modal").style.display = 'none';
+}
+
+// FORM display input/save object
+function saveBook(event) {
+  event.preventDefault();
+  let   currentTitle  = this.querySelector('[name=title-x]').value;
+  let   currentAuthor = this.querySelector('[name=author-x]').value;
+  let   currentPages  = this.querySelector('[name=pages-x]').value;
+  const userBook      = new Book(currentTitle,currentAuthor,currentPages);
+  addBookToLibrary(userBook);
+  document.querySelector(".modal").style.display = 'none';
+   formatBookObject();
+};
 
 function formatBookObject() {
-// console.log(myLibrary[0]);
-myLibrary.forEach(function(book) {
+  let oldLib =  document.getElementById("cardsContainer");
+  body.removeChild(oldLib);
+  let refreshlibrary = document.createElement("div");
+  refreshlibrary.setAttribute("id", "cardsContainer");
+  body.appendChild(refreshlibrary);
 
+  myLibrary.forEach(function(book) {
   let bookCard = document.createElement("div");
-    bookCard.className = "bookCard";
-    cardsContainer.appendChild(bookCard);
+
+  bookCard.className = "bookCard";
+  refreshlibrary.appendChild(bookCard);
+
 // < - - Title ELEMENT - - >
   let actualTitle = document.createElement("h2");
   let divider     = document.createElement("p");
@@ -71,23 +86,39 @@ myLibrary.forEach(function(book) {
     bookCard.appendChild(bookAuthorContainer);
     bookAuthorContainer.appendChild(actualAuthor);
     actualAuthor.textContent = `${book.author}`;
+
+// < - - Buttons ELEMENT - - >
+  let buttonContainer = document.createElement("div");
+  let openButton      = document.createElement("button");
+  let deleteButton    = document.createElement("button");
+  let openIcon        = document.createElement('img');
+  let deleteIcon      = document.createElement('img');
+
+buttonContainer.className = "buttons-container";
+openButton.className      = "open-book";
+deleteButton.className    = "delete-book";
+openIcon.className        = "open-icon";
+deleteIcon.className      = "delete-icon";
+
+bookCard.appendChild(buttonContainer);
+buttonContainer.appendChild(openButton);
+buttonContainer.appendChild(deleteButton);
+openButton.appendChild(openIcon);
+deleteButton.appendChild(deleteIcon);
+
+openIcon.setAttribute("src","images/open-book.svg")
+deleteIcon.setAttribute("src","images/delete.svg")
+
 // < - - Pages ELEMENT - - >
   let bookPagesContainer = document.createElement("div");
-  let actualPages = document.createElement("span");
+  let actualPages        = document.createElement("span");
+
     bookPagesContainer.className = "pagesContainer";
-    actualPages.className = "pages-tag";
+    actualPages.className        = "pages-tag";
+
     bookCard.appendChild(bookPagesContainer);
     bookPagesContainer.appendChild(actualPages);
     actualPages.textContent = `${book.num_page} pages`;
-
-  // let bookReadContainer = document.createElement("div");
-  // let actualRead = document.createElement("span");
-  //   bookReadContainer.className = "readContainer";
-  //   actualRead.className = "readCard";
-  //   bookCard.appendChild(bookReadContainer);
-  //   bookReadContainer.appendChild(actualRead);
-  //   actualRead.textContent = `Have read: ${book.have_read}`;
-
   });
 };
 
